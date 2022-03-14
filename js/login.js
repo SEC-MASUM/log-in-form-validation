6e6f8c3c98c4d7ca8e3292f3e029156c4a8c3fb8
SEC-MASUM <masum.jess42@gmail.com>
2022-03-14 13:10:50 +0600

    check valid or invalid email and show error message in green or red text

--- js/login.js
+++ js/login.js
@@ -1,10 +1,46 @@
+document.getElementById("invalid-email").style.display = "none";
+document.getElementById("valid-email").style.display = "none";
+
+/* Using regular expressions is probably the best way. You can see a bunch of tests here (taken from chromium) */
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
 const loginButton = document.getElementById("login-button");
 loginButton.addEventListener("click", function () {
   const emailField = document.getElementById("email");
   const passwordField = document.getElementById("password");
   const emailText = emailField.value;
   const passwordText = passwordField.value;
-  if (emailText === "sontan@baap.com" && passwordText === "secret") {
-    window.location.href = "banking.html";
+  if (validateEmail(emailText) && passwordText) {
+    alert("Successful your login");
+  }
+  if (validateEmail(emailText)) {
+    document.getElementById("valid-email").style.display = "block";
+    document.getElementById("invalid-email").style.display = "none";
+  } else {
+    document.getElementById("valid-email").style.display = "none";
+    document.getElementById("invalid-email").style.display = "block";
   }
 });
+
+const validate = () => {
+  var x = document.getElementById("email");
+  x.value = x.value.toLowerCase();
+  const emailField = document.getElementById("email");
+  const passwordField = document.getElementById("password");
+  const emailText = emailField.value;
+  const passwordText = passwordField.value;
+  if (validateEmail(emailText)) {
+    document.getElementById("valid-email").style.display = "block";
+    document.getElementById("invalid-email").style.display = "none";
+  } else {
+    document.getElementById("valid-email").style.display = "none";
+    document.getElementById("invalid-email").style.display = "block";
+  }
+};
